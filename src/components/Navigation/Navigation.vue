0c536f3cf46fca559809bbe865313b8fb0d6e23d
ameyaaklekar <ameyaaklekar@gmail.com>
2021-06-20 21:20:54 +1200

    first commit

---
+++ src/components/Navigation/Navigation.vue
@@ -0,0 +1,100 @@
+<template>
+  <nav>
+    <Sidebar v-bind:drawer="drawer" v-bind:mini="mini" />
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="() => { drawer = !drawer; mini = false }"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <NotificationMenu />
+
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :nudge-width="150"
+        offset-y
+        left
+        bottom
+        transition="slide-y-transition"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-account-cog</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <img
+                  src="https://cdn.vuetifyjs.com/images/john.jpg"
+                  alt="John"
+                >
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>John Leider</v-list-item-title>
+                <v-list-item-subtitle>Founder of Vuetify</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list>
+            <v-list-item 
+              v-for="item in settingsMenu" :key="item.title"
+              link 
+              :to="item.url">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+import NotificationMenu from './NotificationMenu.vue'
+import Sidebar from './Sidebar.vue'
+export default {
+  name: 'Navigation',
+  components: { 
+    Sidebar,
+    NotificationMenu 
+  },
+  data: () => ({
+    message: true,
+    hints: true,
+    drawer: null,
+    mini: false,
+    menu: false,
+    settingsMenu: [
+      {
+        title: 'Profile',
+        url: '/profile'
+      },
+      {
+        title: 'Company Profile',
+        url: '/suppliers'
+      },
+      {
+        title: 'Preferences',
+        url: '/stock'
+      }
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navigation/Sidebar.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    :mini-variant.sync="mini"
+    app
+  >
+    <v-list-item class="px-2">
+      <v-list-item-avatar>
+        <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+      </v-list-item-avatar>
+
+      <v-list-item-title>John Leider</v-list-item-title>
+
+      <v-btn
+        icon
+        @click.stop="mini = !mini"
+      >
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list>
+      <v-list-item 
+        v-for="item in menuItems" :key="item.title"
+        link 
+        :to="item.url">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  props: {
+    drawer: Boolean, 
+    mini: Boolean
+  },
+  data: () => ({ 
+    menuItems: [
+      {
+        title: 'Dashboard',
+        icon: 'mdi-view-dashboard',
+        url: '/'
+      },
+      {
+        title: 'Suppliers',
+        icon: 'mdi-factory',
+        url: '/suppliers'
+      },
+      {
+        title: 'Stock',
+        icon: 'mdi-store',
+        url: '/stock'
+      },
+      {
+        title: 'Employees',
+        icon: 'mdi-account-group',
+        url: '/employees'
+      },
+    ]
+  })
+}
+</script>
+
+<style>
+
+</style>
